f1092b1cd5fd07ffe00486696c5ae6823efefd06
Jet <jetflyer1130@gmail.com>
2024-04-25 19:45:52 -0400

    stuff

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import { app, analytics, provider, db } from './utils/firebase'; // eslint-disable-line no-unused-vars
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://504d5dea83dbe7214adfb662b7551e01@o4506644997341184.ingest.us.sentry.io/4507149784449024",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", "https://blogger-gamma-flax.vercel.app/"],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,7 +26,3 @@ root.render(
 		<App />
 	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
